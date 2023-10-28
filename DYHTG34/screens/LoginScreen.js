6ba79f56beb9a61944fbecff315798046f43b429
Renee Wallis <2560885w@student.gla.ac.uk>
2023-10-28 17:24:51 +0100

    centered login page

--- DYHTG34/screens/LoginScreen.js
+++ DYHTG34/screens/LoginScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, TextInput, Button, SafeAreaView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TextInput, Button, SafeAreaView, TouchableOpacity, View } from 'react-native';
 import React, { useState} from 'react';
 import { setCustomerId } from '../components/CustomerID';
 import { useNavigation } from '@react-navigation/native';
 import { Input } from '../node_modules/react-native-elements/dist/index';
+import { Padding } from '../GlobalStyles';
 
 
 const LoginScreen = () => {
@@ -12,7 +13,7 @@ const LoginScreen = () => {
     const navigation = useNavigation();
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.pageContainer}>
             <Text>Login</Text>
             <TextInput
                 label="Username"
@@ -21,18 +22,28 @@ const LoginScreen = () => {
                 placeholder='Enter username'
                 onChangeText={(value) => { setEmail(value) }} />
 
-            <TextInput
-                label="Password"
-                style={styles.textInput}
-                secureTextEntry={isSecureEntry}
-                value={password}
-                placeholder='Enter Password'
-                onChangeText={(value) => { setPassword(value) }} />
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    label="Password"
+                    style={styles.passwordInput}
+                    secureTextEntry={isSecureEntry}
+                    value={password}
+                    placeholder='Enter Password'
+                        onChangeText={(value) => { setPassword(value) }} />
+                    <TouchableOpacity onPress={(isSecureEntry) => setIsSecureEntry(!isSecureEntry)}>
+                        <Text>{isSecureEntry ? 'show' : 'hide'}</Text>
+                    </TouchableOpacity>
+
+
+
+
+            </View>
 
             <Button
                 onPress={() => { this.onPress(navigation, email, password); setEmail(''); setPassword('') }}
                 title='Log in'
-            />
+                />
+            
         </SafeAreaView>
     );
 };
@@ -64,11 +75,40 @@ let checkemail = async (userEmail) => {
 
 
 const styles = StyleSheet.create({
+    pageContainer: {
+        padding: 20,
+        justifyContent: 'center',
+        flex: 1
+    },
     textInput: {
+        height : 50,
         fontSize: 20,
         borderWidth: 2,
         borderColor: 'blue',
-        margin: 10
+        paddingTop: 20,
+        marginHorizontal: 12,
+        marginVertical: 20,
+        padding: 10
+    },
+    passwordInput: {
+        height: 40,
+        fontSize: 20,
+        borderWidth: 2,
+        borderColor: 'white',
+        margin: 12,
+        flex: 1
+    },
+
+    passwordContainer: {
+        height: 50,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'white',
+        borderColor: 'blue',
+        borderWidth: 2,
+        marginHorizontal: 12,
+        marginVertical: 20
     }
 });
 
