b8306ab90c4bf8f0263379854f58ed5f042fa301
ewan <ewan@ubuntu.myguest.virtualbox.org>
2023-10-28 10:34:56 +0100

    Created basic return for ProductScreen based on logic in OrderScreen, no products returned yet

--- DYHTG34/screens/ProductScreen.js
+++ DYHTG34/screens/ProductScreen.js
@@ -1,4 +1,5 @@
-
+import React, { useState, useEffect } from 'react';
+import { ActivityIndicator } from 'react-native';
 
 const ProductScreen = () => {
   const [data, setData] = useState([]);
@@ -16,5 +17,18 @@ const ProductScreen = () => {
     }
     setLoading(false);
   }
+  
+  useEffect(() => {
+      fetchProducts();
+  }, []);
 
+  return (
+    <View style={style.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <Text> Loaded </Text> // make this loop through every product and display a product component
+      )}
+    </View>
+  )
 }
